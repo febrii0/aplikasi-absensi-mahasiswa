98ae858767824264d8fb06beca5be8c6e48be055
DwiEllya27 <dwie5736@gmail.com>
2023-07-10 17:52:54 +0700

    update tampilan absensi dan status kehadiran

--- app/Views/absensi/status.php
+++ app/Views/absensi/status.php
@@ -1,25 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Status Kehadiran</h1>
-    <table border = "1" colspacing = "0" cellpadding = "5">
-        <tr>
-            <th>No.</th>
-            <th>Status</th>
-        </tr>
-        <?php $i = 1; ?>
-        <?php foreach($data_status as $row) :?>
-            <tr>
-                <td><?= $i++; ?> </td>
-                <td><?php echo $row['status']?></td>
-            </tr>
-        <?php endforeach; ?>
-    </table>
-</body>
-</html>
+<?= $this->extend('layout/layout') ?>
+<?= $this->section('content') ?>
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-2 text-gray-800">Status Kehadiran</h1>
+
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Status Kehadiran</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <tr>
+                            <th>No</th>
+                            <th>Status</th>
+                        </tr>
+                    <?php $i = 1; ?>
+                <?php foreach ( $data_status as $status) : ?>
+                    <tr>
+                        <td> <?= $i++; ?></td>
+                        <td> <?= $status["status"] ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+
+        </div>
+    </div>
+</div>
+<?= $this->endSection() ?>
