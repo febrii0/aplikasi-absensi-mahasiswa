376c2d2d99a146d8ac04f486323492d4430678aa
DwiEllya27 <dwie5736@gmail.com>
2023-07-10 14:33:01 +0700

    update MahasiswaModel.php

--- app/Models/MahasiswaModel.php
+++ app/Models/MahasiswaModel.php
@@ -6,7 +6,7 @@ use CodeIgniter\Model;
 class MahasiswaModel extends Model
 {
     protected $table = 'mahasiswa';
-    protected $primaryKey = 'id';
+    protected $primaryKey = 'id_mhs';
     protected $useAutoIncrement = true;
     protected $allowFields = [];
 
@@ -34,27 +34,7 @@ class MahasiswaModel extends Model
     { 
        return $this-> findAll();
     }
-    //Fungsi ini digunakan untuk mencari data berdasarkan ID
-    public function getDataByID($id)
-    {
-        return $this->find($id);
-    }
-    //Fungsi ini digunakan untuk melakukan query dengan kondisi WHERE
-    public function getDataBy($data)
-    {
-        return $this->where('genre', $data)->findAll();
-    }
-    //digunakan untuk mengurutkan hasil query berdasarkan kolom tertentu
-    public function getOrderBy()
-    {
-        return $this->OrderBy('created_at', 'desc')->findAll();
-    }
-    //digunakan untuk membatasi jumlah data yang diambil dari tabel
-    public function getLimit()
-    {
-        return $this->limit(5)->findAll();
-    }
-
+    
 }
 
 
