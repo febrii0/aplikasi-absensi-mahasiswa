f6e5383d08d17bd755e5d802f61d601aed168ffe
Febriansyah Agung Tirta <febriansyahagungtirta@gmail.com>
2023-07-10 20:22:19 +0700

    update dashboard

--- app/Views/home/dashboard.php
+++ app/Views/home/dashboard.php
@@ -59,17 +59,7 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">Mahasiswa Tidak Hadir
                             </div>
-                            <div class="row no-gutters align-items-center">
-                                <div class="col-auto">
-                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
-                                </div>
-                                <div class="col">
-                                    <div class="progress progress-sm mr-2">
-                                        <div class="progress-bar bg-danger" role="progressbar" style="width: 50%"
-                                            aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                </div>
-                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-user-times fa-2x text-gray-300"></i>
