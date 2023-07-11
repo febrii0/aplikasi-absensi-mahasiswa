d7cdf46a0ac6607a10453009863bd896ef7860bb
callmededi <dedisurahmin@gmail.com>
2023-07-11 13:14:49 +0700

    Login

--- app/Views/menu/login.php
+++ app/Views/menu/login.php
@@ -40,13 +40,13 @@
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Selamat Datang!</h1>
                                     </div>
                                     <form class="user">
                                         <div class="form-group">
-                                            <input type="email" class="form-control form-control-user"
-                                                id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address...">
+                                            <input type="NPM" class="form-control form-control-user"
+                                                id="NPM" aria-describedby="NPMHelp"
+                                                placeholder="Enter Your NPM...">
                                         </div>
                                         <div class="form-group">
                                             <input type="password" class="form-control form-control-user"
@@ -54,13 +54,10 @@
                                         </div>
                                         <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Remember
-                                                    Me</label>
                                             </div>
                                         </div>
                                         <a href="index.html" class="btn btn-primary btn-user btn-block">
-                                            Login
+                                            Login Cee
                                         </a>
                                     </form>
                                     <!-- <hr> -->
